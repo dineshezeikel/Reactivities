d673cb45bdb719bba202e3340f55e96bfbf5e172
Dinesh Ezeikel <dinesh.e@Atyati.com>
2020-04-06 21:58:28 +0530

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        //Auto implimented property {where no additional logic is required in property accessor {gettter and setter}}
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+
+    }
+}
